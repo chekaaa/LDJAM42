3059e0b8c37be6cefc9fc1e7cf1afd1a31ea1118
chekaaa <carlos93@hotmail.cl>
2018-08-11 03:23:09 -0400

    Car movement

---
+++ LD42/Assets/Scripts/PhysicObject.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class PhysicsObject : MonoBehaviour
+{
+    [SerializeField] private float accelerationForce = 5f;
+    [SerializeField] protected float torqueForce = 2f;
+    protected float torqueDirection;
+    protected float accelerationDirection;
+    protected float stopThreshold = .7f;
+
+    protected Rigidbody2D rb;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        //Request player inputs
+        ProcessInputs();
+    }
+
+    protected virtual void ProcessInputs() { }
+
+    void FixedUpdate()
+    {
+        //Add the forces and torque to the RigidBody2D
+        Movement();
+    }
+
+    void Movement()
+    {
+        rb.velocity = ForwardVelocity();
+        //Impulse the car forward
+        rb.AddForce(accelerationDirection * accelerationForce * transform.up);
+        //If the car velocity is not between the threshold it will start turning
+        //but if it is the car will smoothly stop turning
+        if (ForwardVelocity().magnitude < -stopThreshold || ForwardVelocity().magnitude > stopThreshold)
+        {
+            rb.angularVelocity = (torqueForce * -torqueDirection);
+        }
+        else
+        {
+            rb.angularVelocity = Mathf.Lerp(rb.angularVelocity, 0f, 2f * Time.deltaTime);
+        }
+
+
+
+    }
+
+    Vector2 ForwardVelocity()
+    {
+        return transform.up * Vector2.Dot(rb.velocity, transform.up);
+    }
+
+    Vector2 SideDirection()
+    {
+        return transform.right * Vector2.Dot(rb.velocity, transform.right);
+    }
+
+}
---
+++ LD42/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : PhysicsObject
+{
+
+    protected override void ProcessInputs()
+    {
+        accelerationDirection = Input.GetAxis("Vertical");
+
+        torqueDirection = Input.GetAxis("Horizontal");
+    }
+}
